cb1a2958494df0ca5b71d1f36d1d12865ac36df5
Rokas Vaitkevicius <rokas.vaitkevicius1@gmail.com>
2020-08-15 14:38:03 +0100

    Added a logo, some fonts, created a pages controller for MY Recipes page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  get "my_recipes", to: "pages#my_recipes"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes do
